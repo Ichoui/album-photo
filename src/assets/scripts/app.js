39b4aaad7a3a0c46d45c755e62a38c8a4bddedef
ichoui <nicol.morgan@outlook.fr>
2018-12-16 17:50:10 +0100

    Ca peut peut être s'optimiser, mais le drag fonctionne vraiment pas mal là

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -8,51 +8,70 @@ $(function () {
 
   ///////// SCROLLBOOSTER /////////
   // let img = content.querySelector('img');
+/*
 
   let sb = new ScrollBooster({
-    viewport,
-    content,
-    emulateScroll: false,
-    onUpdate: (data) => {
-      content.style.transform = `translate(
-      ${-data.position.x}px,
-      ${-data.position.y}px
-    )`
-    }
-  });
-
-  content.addEventListener('load', () => {
-    // update sizes/metrics after image load
-    sb.updateMetrics();
-
-    // set viewport position to the center of an image
-    let offsetX = content.scrollWidth - viewport.offsetWidth;
-    let offsetY = content.scrollHeight - viewport.offsetHeight;
-    sb.setPosition({
-      x: offsetX / 2,
-      y: offsetY / 2
+      viewport,
+      content,
+      emulateScroll: false,
+      onUpdate: (data) => {
+        content.style.transform = `translate(
+        ${-data.position.x}px,
+        ${-data.position.y}px
+      )`
+      },
+      shouldScroll: (data, event) => {
+        console.log(event);
+        return true
+        // if (event.target.classList.contains('images')) {
+        // }
+      },
     });
-  });
 
-  // TEST
+    console.log(sb);
+    content.addEventListener('load', () => {
+      // update sizes/metrics after image load
+      sb.updateMetrics();
+      // set viewport position to the center of an image
+      let offsetX = content.scrollWidth - viewport.offsetWidth;
+      let offsetY = content.scrollHeight - viewport.offsetHeight;
+      sb.setPosition({
+        x: offsetX / 8,
+        y: offsetY / 8
+      });
+    });
+*/
 
-  let tabImg = [];
-  $('.images').each(function () {
-    let $this = $(this);
-    let elem = $this[0];
 
-    // elem.css({})
-
-  });
+  // console.log($('.img-container').height());
+  // console.log($('.img-container').width());
 
   ///////// MASONRY /////////
 
-  $('.img-container').masonry({
+ $('.wrapper').masonry({
     itemSelector: '.images',
     columnWidth: 400,
     gutter: 5
   });
 
   ///////// APPLICATION /////////
-  body.css({'overflow':'hidden'});
+  body.css({'overflow': 'hidden'});
+
+
+  var $container = $(".viewport");
+  var $scroller = $(".img-container");
+  bindDragScroll($container, $scroller);
+
+  // Images width height
+  const img = $('.images');
+  const imgW = img.width();
+  const imgH = img.height();
+
+  // Masonry maison si jamais pour gérer orientation image
+  img.each(function(e) {
+    const $this = $(this);
+    // console.log($this);
+  })
+
+
 });
