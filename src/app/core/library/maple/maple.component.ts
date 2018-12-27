85ab2b1f973cca0ccf1e870425bf2d8fbb446b99
ichoui <nicol.morgan@outlook.fr>
2018-12-27 18:06:04 +0100

    J'en ai marre, sérieux

--- src/app/core/library/maple/maple.component.ts
+++ src/app/core/library/maple/maple.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { RestService } from '../../../config/api/rest.service';
 import ScrollBooster from 'scrollbooster';
 import { ScriptService } from '../../../config/scripts/scripts.service';
+// declare var $:JQueryStatic;
+declare var jQuery: any;
 
 
 @Component({
@@ -18,47 +20,40 @@ export class MapleComponent implements OnInit {
   public images$ = this.imgService.getMaple();
   hideScrollbar = true;
 
-  constructor(private imgService: RestService, private script:ScriptService) {
+  constructor(private imgService: RestService, private script: ScriptService) {
   }
 
   ngOnInit() {
     this.scrollBooster();
     this.script.load('app').then(data => data);
-    let images = document.querySelectorAll('.grid');
-    console.log(images.clientHeight);
-    // essayer de renverse portrait image
-
-
+    let content = document.querySelector('.content');
+    setTimeout(function () {
+      console.log(test.offsetHeight);
+      // content['style'].minHeight = content.offsetHeight + 'px';
+      content['style'].maxHeight = "250px";
+    }, 1500);
   }
 
   scrollBooster() {
     let viewport = document.querySelector('.viewport');
     let content = document.querySelector('.content');
-/*
+
     let sb = new ScrollBooster({
       viewport: viewport, // required
       content: content, // required
+      bounce: false,
+      emulateScroll: false,
 
-      bounce: true,
+      textSelection: false,
       onUpdate: (data) => {
         // console.log(data);
-        content.style.transform = `translate(
-      ${-data.position.x}px,
-      ${-data.position.y}px )`;
+        content['style'].transform = `translate3d(
+          ${-data.position.x}px,
+          ${-data.position.y}px, 0 )`;
+
       },
 
     });
-*/
-  }
-
-  leftBoundStat(reachesLeftBound: boolean) {
-    console.log(reachesLeftBound);
-    this.leftNavDisabled = reachesLeftBound;
-  }
-
-  rightBoundStat(reachesRightBound: boolean) {
-    console.log(reachesRightBound);
-    this.rightNavDisabled = reachesRightBound;
   }
 
 
--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,5 +1,30 @@
 $(function () {
 
   ///////// GENERAL VARIABLES /////////
+ let img = $('.images');
+
+/* let $grid = $('.grid');
+  console.log($grid);
+  setTimeout(function () {
+    let contentH = $grid[0].clientHeight;
+    console.log(contentH);
+*/
+  ///////// APPLICATION /////////
+
+
+  console.log($('.images'));
+  // Images width height
+  // Pour gérer orientation images et donc width
+  img.each(function (e) {
+    console.log(e);
+    const $this = $(this);
+    // console.log($this[0].width);
+    if ($this[0].width > $this[0].height) {
+      $this.addClass('widther');
+    } else {
+      $this.addClass('heigher');
+    }
+  })
+
 
 });
